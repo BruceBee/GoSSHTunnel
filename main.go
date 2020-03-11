7c1ad2d368717411727442679bd8471ea4b9d45a
Bruce Bee <mzpy_1119@126.com>
2020-03-11 14:05:04 +0800

    Update main.go

--- main.go
+++ main.go
@@ -53,7 +53,12 @@ func main()  {
 	// Create the Signer for this private key.
 	signer, err := ssh.ParsePrivateKeyWithPassphrase(key, []byte(config.SSH.Pwd))
 	if err != nil {
-		log.Fatalf("unable to parse private key: %v", err)
+		//log.Fatalf("1unable to parse private key: %v", err)
+
+		signer, err = ssh.ParsePrivateKey(key)
+		if err != nil {
+			log.Fatalf("2unable to parse private key: %v", err)
+		}
 	}
 
 	conf := &ssh.ClientConfig{
